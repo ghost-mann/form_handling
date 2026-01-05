6bef8f18b5272a5f33cd4ca8c9d340354e18aea8
ghost-mann <iamaob8@protonmail.com>
2026-01-05 19:42:30 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+
+</head>
+<body>
+<form action="index.php" method="post">
+Name: <input type="text" name="name"><br>
+Email: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
